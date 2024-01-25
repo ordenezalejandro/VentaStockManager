efc4c1ca64ca67a4ee0246fe1e7745594987033a
jairo <jairo222@gmail.com>
2024-01-24 21:23:02 -0300

    template

--- VentaStockManager/Cliente/urls.py
+++ VentaStockManager/Cliente/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import filtrar_por_mayor_de_edad
+from .views import filtrar_por_mayor_de_edad, filtrar_por_menor_de_edad,filtrar_por_de_18,mostrar_todos_los_clientes
+
 
 urlpatterns = [
     path('mayor_de_edad/', filtrar_por_mayor_de_edad, name='filtrar_por_mayor_de_edad'),
+    path('menor_de_edad/', filtrar_por_menor_de_edad, name='filtrar_por_menor_de_edad'), 
+    path('de_18/', filtrar_por_de_18, name='filtrar_por_de_18'),
+    path('mostrar_todos_los_clientes/', mostrar_todos_los_clientes, name='clientes'), 
 ]
--- VentaStockManager/Cliente/views.py
+++ VentaStockManager/Cliente/views.py
@@ -3,7 +3,22 @@ from Cliente.models import Cliente
 from django.http import HttpResponse
 
 
-# Create your views here.
+#Cliente/cliente/ Create your views here.
 def filtrar_por_mayor_de_edad(request):
     clientes_mayores = Cliente.objects.filter(edad__gte=18) 
     return HttpResponse(f"Clientes mayores de edad: {''.join(['<p>' + str(cliente) + '</p>' for cliente in clientes_mayores])}")
+
+# Create your views here.
+def filtrar_por_menor_de_edad(request):# Create your views here.
+    clientes_menores = Cliente.objects.filter(edad__lte=18) 
+    return render(request, 'clientes.html', {'clientes': clientes_menores})
+
+def filtrar_por_de_18 (request):# Create your views here.
+    clientes_de_18 = Cliente.objects.filter(edad=18) 
+    return render(request, 'clientes.html', {'clientes': clientes_de_18})
+    
+def mostrar_todos_los_clientes(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes.html', {'clientes': clientes})
+
+
