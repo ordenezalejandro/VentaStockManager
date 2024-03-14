3cc930fed47455c46885040a675742e43c0dd2ad
jairoDO <jairo222@gmail.com>
2024-03-14 20:06:47 -0300

    updte VentaStockManger

--- VentaStockManager/VentaStockManager/settings.py
+++ VentaStockManager/VentaStockManager/settings.py
@@ -139,9 +139,11 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Heroku: Actualice la configuración de la base de datos desde $DATABASE_URL.
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 # DATABASES = {
 #     'default': dj_database_url.config(
 #         # Heroku proporciona la URL de la base de datos a través de variables de entorno
