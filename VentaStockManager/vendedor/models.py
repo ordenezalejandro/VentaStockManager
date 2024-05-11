a2f4129a128912efb22ef5ed7a8ecda72a4c1f8f
jairo ordonez <ordonezgabrielalejandro2@gmail.com>
2024-05-11 14:28:09 -0300

    venta por vendedor funcionando

--- VentaStockManager/vendedor/models.py
+++ VentaStockManager/vendedor/models.py
@@ -19,7 +19,7 @@ class Vendedor(models.Model):
   # Campos adicionales del vendedor (opcional)
   # Ej: nombre_completo, telefono, etc.
     def fullname(self):
-            return self.nombre + (' ') + self.apellido
+            return self.nombre + ' ' + self.apellido
 
     def __str__(self):
         return self.usuario.username
