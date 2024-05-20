c0e3755b0ac49be3f382ccec2e52beddf824a011
jairoDO <jairo222@gmail.com>
2024-05-20 13:02:19 -0300

    add material setting

--- VentaStockManager/VentaStockManager/settings.py
+++ VentaStockManager/VentaStockManager/settings.py
@@ -130,18 +130,26 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 MATERIAL_ADMIN_SITE = {
-    'SHOW_THEMES': False,
-    'TRAY_REVERSE': True,
-    'NAVBAR_REVERSE': True,
-    'SHOW_COUNTS': True,
-    'APP_ICONS': {
+    # 'HEADER':  _('Your site header'),  # Admin site header
+    # 'TITLE':  _('Your site title'),  # Admin site title
+    # 'FAVICON':  'path/to/favicon',  # Admin site favicon (path to static should be specified)
+    # 'MAIN_BG_COLOR':  'color',  # Admin site main color, css color should be specified
+    # 'MAIN_HOVER_COLOR':  'color',  # Admin site main hover color, css color should be specified
+    # 'PROFILE_PICTURE':  'path/to/image',  # Admin site profile picture (path to static should be specified)
+    # 'PROFILE_BG':  'path/to/image',  # Admin site profile background (path to static should be specified)
+    # 'LOGIN_LOGO':  'path/to/image',  # Admin site logo on login page (path to static should be specified)
+    # 'LOGOUT_BG':  'path/to/image',  # Admin site background on login/logout pages (path to static should be specified)
+    'SHOW_THEMES':  True,  #  Show default admin themes button
+    'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
+    'NAVBAR_REVERSE': True,  # Hide side navbar by default
+    'SHOW_COUNTS': True, # Show instances counts for each model
+    'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
         'sites': 'send',
     },
-    'MODEL_ICONS': {
+    'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
         'site': 'contact_mail',
     }
 }
-
 # settings.py
 
 # Configuración de HSTS (HTTP Strict Transport Security)
