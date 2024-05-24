b29e65112b871729f8961750da34deeb1fc58c1c
jairoDO <jairo222@gmail.com>
2024-05-24 12:07:33 -0300

    add pre loaded form values

--- VentaStockManager/vendedor/models.py
+++ VentaStockManager/vendedor/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Vendedor(models.Model):
-    GENERO_CHOICES = [
+    GENERO_CHOICES = [ 
         ('M', 'Masculino'),
         ('F', 'Femenino'),
     ]
