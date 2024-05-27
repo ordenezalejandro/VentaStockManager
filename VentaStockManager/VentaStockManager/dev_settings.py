840343604cc1f8ce0d911a048bfa75d1f1aef2ce
jairoDO <jairo222@gmail.com>
2024-05-27 19:27:39 -0300

    add settings

---
+++ VentaStockManager/VentaStockManager/dev_settings.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+# Application definition    
+INSTALLED_APPS = [
+    'material',
+    'material.admin',
+    # 'material.admin.default',
+    'whitenoise.runserver_nostatic',    
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    #'django.contrib.staticfiles',
+    'bootstrap5',
+    'dal',
+    'dal_select2',
+    'django_extensions',
+    'cliente.apps.ClienteConfig',
+    'venta.apps.VentaConfig',
+    'articulo.apps.ArticuloConfig',
+    'vendedor.apps.VendedorConfig',
+    'compra.apps.CompraConfig',
+]
+
+DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
