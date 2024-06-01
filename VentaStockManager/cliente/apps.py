36e1843aa064a4581f1784451e9ce88b9cdeca51
jairoDO <jairo222@gmail.com>
2024-05-31 22:23:24 -0300

    add icons

--- VentaStockManager/cliente/apps.py
+++ VentaStockManager/cliente/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class ClienteConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "cliente"
+    icon_name = "account_circle"
     
--- VentaStockManager/compra/apps.py
+++ VentaStockManager/compra/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CompraConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "compra"
+    icon_name = "shopping_cart"
