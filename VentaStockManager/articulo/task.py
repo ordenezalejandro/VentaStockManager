241cce38e7e419dfdd45ccd36415dab09f305683
jairoDO <jairo222@gmail.com>
2024-09-17 01:31:33 -0300

    algo

--- VentaStockManager/articulo/task.py
+++ VentaStockManager/articulo/task.py
@@ -135,13 +135,13 @@ def procesar_archivo_xlsx(ruta_archivo):
     return errores  
 
 def actualizar_precios_articulos_desde_drive():
-    ruta_archivo = download_sheet_from_google_sheets(file_id, 'articulos!A1:Z1500', 'articulo/data/')
+    ruta_archivo = download_sheet_from_google_sheets(FILE_ID, 'articulos!A1:Z1500', 'articulo/data/')
     if ruta_archivo and os.path.exists(ruta_archivo):
         errores = procesar_archivo_xlsx(ruta_archivo)
         if errores:
             return errores
         else:
-            return "Se actualizar los precios desde e archvo drive con exito"
+            return "Se actualizaron los precios desde el archivo drive con éxito"
     else:
         return f"Archivo no encontrado en {ruta_archivo}"
 def generar_codigo_interno(nombre):
