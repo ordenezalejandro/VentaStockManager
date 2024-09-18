91c37015e2c7fcb0d29d042f009bab7718be4038
jairoDO <jairo222@gmail.com>
2024-09-18 20:14:39 -0300

    add dd

--- VentaStockManager/venta/views.py
+++ VentaStockManager/venta/views.py
@@ -324,7 +324,7 @@ def generar_pdf_pedidos(request, pedido_ids=None):
     styleN = ParagraphStyle(
         'Normal',
         parent=styles['Normal'],
-        fontSize=26,  # Increase font size
+        fontSize=30,  # Increase font size
         fontName='Helvetica-Bold'  # Set font to bold
     )
     padding = 1 * cm  # Increase padding
@@ -350,8 +350,10 @@ def generar_pdf_pedidos(request, pedido_ids=None):
             articulo_info = f"{articulo_venta.articulo.get_articulo_short_name()}" 
             if len(articulo_info) > 40:
                 articulo_info = articulo_info[:40] + '\n' + articulo_info[40:]
-            articulo_info += f" \n ({articulo_venta.cantidad} x ${articulo_venta.precio})   ${articulo_venta.total}\n"
+                
+            articulo_info_price += f" \t ({articulo_venta.cantidad} x ${articulo_venta.precio})   ${articulo_venta.total}\n"
             elements.append(Paragraph(articulo_info, styleN))
+            elements.append(Paragraph(articulo_info_price, styleN))
             elements.append(Spacer(1, padding))
 
         # Línea después de los artículos
