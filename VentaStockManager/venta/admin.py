2093e2fbf77812d73dc285c3a912ca06dc273666
jairoDO <jairo222@gmail.com>
2024-09-18 19:13:53 -0300

    hotfix

--- VentaStockManager/venta/admin.py
+++ VentaStockManager/venta/admin.py
@@ -87,7 +87,7 @@ class VentaAdmin(admin.ModelAdmin):
 
     def total_venta_por_articulo(self, obj):
         total = 0
-        for articulo_venta in obj.venta.ventas.all():  # Corrected line
+        for articulo_venta in obj.articulos.all():
             precio = articulo_venta.precio.replace("'", "").replace(",", "")
             total += articulo_venta.cantidad * float(precio)
         return total
@@ -140,7 +140,7 @@ class PedidoAdmin(admin.ModelAdmin):
 
     def total_venta_por_articulo(self, obj):
         total = 0
-        for articulo_venta in obj.articulos.all():
+        for articulo_venta in obj.venta.articulos.all():
             precio = articulo_venta.precio.replace("'", "").replace(",", "")
             total += articulo_venta.cantidad * float(precio)
         return total
